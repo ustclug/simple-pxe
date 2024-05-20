a8928d9865f83f5a38b5c9295ba3a9ebf48d1414
taoky <taoky@ustclug.org>
2024-05-21 05:15:05 +0800

    Fix memtest86+

--- menu.d/tool.sh
+++ menu.d/tool.sh
@@ -7,11 +7,11 @@ source functions.sh
 do_memtest() {
 	cd "${LOCAL_PATH}/memtest" || return 1
 
-	binary=$(ls memtest86_*_x64.efi | sort -ru | head -n1)
+	binary=$(ls memtest.efi | sort -ru | head -n1)
 	if [[ -f "${binary}" ]]; then
 		cat <<- EOF
 			if [ \$grub_platform = efi ]; then
-			  menuentry 'Memtest86 (proprietary software)' {
+			  menuentry 'Memtest86+ (EFI)' {
 			    echo 'Loading...'
 			    chainloader ${GRUB_PATH}/memtest/${binary}
 			  }
@@ -19,11 +19,11 @@ do_memtest() {
 		EOF
 	fi
 
-	binary=$(ls memtest86plus_*.bin | sort -ru | head -n1)
+	binary=$(ls memtest.bin | sort -ru | head -n1)
 	if [[ -f "${binary}" ]]; then
 		cat <<- EOF
 			if [ \$grub_platform = pc ]; then
-			  menuentry 'Memtest86+' {
+			  menuentry 'Memtest86+ (BIOS)' {
 			    echo 'Loading...'
 			    linux16 ${GRUB_PATH}/memtest/${binary}
 			}
@@ -36,20 +36,18 @@ do_uefishell() {
 	cd "${PXE_LOCAL_ROOT}/archlinux" || return 1
 	local latest=$(ls | sort -ru)
 
-	for ver in v2 v1; do
-		efipath="${latest}/EFI/shellx64_${ver}.efi"
-		if [[ -f "${efipath}" ]]; then
-			relpath=$(realpath --relative-to="${PXE_LOCAL_ROOT}" "${efipath}")
-			cat <<- EOF
+	efipath="${latest}/EFI/BOOT/BOOTx64.efi"
+	if [[ -f "${efipath}" ]]; then
+		relpath=$(realpath --relative-to="${PXE_LOCAL_ROOT}" "${efipath}")
+		cat <<- EOF
 				if [ \$grub_platform = efi ]; then
-				  menuentry 'UEFI Shell (${ver})' {
+				  menuentry 'UEFI Shell' {
 				    echo 'Loading...'
 				    chainloader $(url2grub "${PXE_HTTP_ROOT}/${relpath}")
-				  }
+			    }
 				fi
 			EOF
-		fi
-	done
+	fi
 }
 
 do_memtest
