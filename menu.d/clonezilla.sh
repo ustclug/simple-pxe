19d794d3ad4f61134c2f7b34c2144e95439200e8
taoky <taoky@ustclug.org>
2024-05-21 14:03:34 +0800

    Fix Clonezilla

--- menu.d/clonezilla.sh
+++ menu.d/clonezilla.sh
@@ -16,7 +16,7 @@ while read -r folder; do
 		"Clonezilla Live ${version} (${arch})" \
 		"${GRUB_PATH}/${kernels[0]}" \
 		"${GRUB_PATH}/${initrds[0]}" \
-		"boot=live netboot=nfs nfsroot=${PXE_NFS_HOST}:${NFS_PATH}/${folder} union=overlay username=user hostname=clonezilla config quiet components noswap edd=on nomodeset noeject ocs_live_run=ocs-live-general ocs_live_extra_param= ocs_live_batch=no vga=788 ip= net.ifnames=0 splash i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.enable_fbdev=1"
+		"boot=live netboot=nfs nfsroot=${PXE_NFS_HOST}:${NFS_PATH}/${folder} union=overlay username=user hostname=clonezilla config quiet components noswap edd=on nomodeset noeject ocs_live_run=ocs-live-general ocs_live_extra_param= ocs_live_batch=no vga=788 net.ifnames=0 splash i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.enable_fbdev=1"
 done < <(ls | sort -t_ -k1,1r)
 
 # vim: set ts=4 sw=4 sts=4 noexpandtab nosta:
